12064127f69d518078a3b2ad8be35114c8ddba4e
Bartosz <weivan321@gmail.com>
2021-07-19 22:27:47 +0200

    changed to setinterval

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,7 @@ const settingsPanel = document.querySelector('.settings');
 const saveBtn = document.querySelector('.btn-save');
 const workSetting = document.querySelector('.work-setting');
 const breakSetting = document.querySelector('.break-setting');
+// const allSettings = document.querySelectorAll('.settings-setting');
 
 // PLAY/PAUSE
 const playBtn = document.querySelector('.btn-start');
@@ -32,9 +33,9 @@ const handleTodo = () => {
 let time = workSetting.value * 60;
 let counter = true;
 
+
 const updateTime = () => {
     if(counter) {
-        playBtn.setAttribute('disabled', true);
         const min = Math.floor(time / 60);
         const minutes = min < 10 ? `0${min}` : `${min}`;
         const sec = Math.floor(time % 60);
@@ -42,31 +43,57 @@ const updateTime = () => {
         
         clockWatch.textContent =`${minutes}:${seconds}`
     } else {
-        counter = !counter;
+            counter = !counter;
+            console.log('kurcze')
             time = workSetting.value * 60;
-            playBtn.removeAttribute('disabled');
-        return
+            return
     }
     
-    setTimeout(() => {
-        if(time > 0) {
-            time--;
-            updateTime()
-        }else {
-            clockWatch.textContent = '00:00';
-            counter = !counter;
-            clearTimeout();
-            updateTime()
-        }
-    }, 1000)
+    // setTimeout(() => {
+    //     if(time > 0 && counter) {
+    //         time--;
+    //         updateTime()
+    //     }else {
+    //         clockWatch.textContent = '00:00';
+    //         counter = !counter;
+    //         clearTimeout();
+    //         updateTime()
+    //     }
+    // }, 100)
 };
 
+let counterInterval;
+const countTime = () => {
+    playBtn.setAttribute('disabled', true);
+    counterInterval = setInterval(() => {
+        if(counter) {
+            time--;
+            updateTime()
+        } else if(time == 0) {
+            clockWatch.textContent = '00:00';
+            counter = !counter;
+            console.log('kraksa')
+            updateTime()
+        }
+    }, 10)
+}
+
 const updateSettings = () => {
-    settingsPanel.classList.remove('active-modal')
-    
+    settingsPanel.classList.remove('active-modal');
+    time = workSetting.value * 60;
+    clockWatch.textContent = workSetting.value > 9 ? `${workSetting.value}:00` : `0${workSetting.value}:00`;
+    counter = !counter;
+    clearInterval(counterInterval);
 };
+
+const pauseTime = () => {
+    playBtn.removeAttribute('disabled');
+    clearInterval(counterInterval)
+
+}
 
 settingsBtn.addEventListener('click', handleSettings);
 saveBtn.addEventListener('click',updateSettings);
 todoBtn.addEventListener('click', handleTodo);
-playBtn.addEventListener('click', updateTime);
+playBtn.addEventListener('click', countTime);
+pauseBtn.addEventListener('click',pauseTime);
